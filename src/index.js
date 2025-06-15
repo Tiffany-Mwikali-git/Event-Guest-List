7cd8bcc5902336041fcaaa8b897d9003aac40acc
Tiffany <marcy.kilonzo@student.moringaschool.com>
2025-06-15 16:12:55 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const guestList = []
+
+function addGuest() {
+    if (guestList.length == 10) {
+        window.alert ('the list is full')
+        return;
+    }
+    const guestInput = document.getElementById("guestInput");
+    const guestName = guestInput.value.trim();
+
+    if (guestName) {
+        guestList.push(guestName);
+        guestInput.value = " "; //if user enters an empty 
+        displayGuest();
+    }  
+}
+
+function displayGuest() {
+    const guestListElement = document.getElementById("guestList");
+    guestListElement.innerHTML = " ";
+
+    guestList.forEach((guest, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = guest;
+
+        const removeBtn = document.createElement("button");
+        removeBtn.textContent = "Remove";
+        removeBtn.onclick = () => removeGuest(index);
+
+        listItem.appendChild(removeBtn);
+        guestListElement.appendChild(listItem);
+    })
+}
+
+function removeGuest(index) {
+    guestList.splice(index , 1);
+    displayGuest();
+    console.log(guestList)
+}
