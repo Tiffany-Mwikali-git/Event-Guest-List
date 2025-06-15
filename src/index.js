abdf241416e759d294096f02ce08fa65419f8f0b
Tiffany <marcy.kilonzo@student.moringaschool.com>
2025-06-15 21:25:10 +0300

    Dom Manipulation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const guestList = []
 
+
 function addGuest() {
+    //making sure the guests dont exceed 10 guests
     if (guestList.length == 10) {
         window.alert ('the list is full')
         return;
@@ -9,28 +11,29 @@ function addGuest() {
     const guestName = guestInput.value.trim();
 
     if (guestName) {
-        guestList.push(guestName);
-        guestInput.value = " "; //if user enters an empty 
+        guestList.push(guestName); //adding a guest using the push method
+        guestInput.value = " "; 
         displayGuest();
     }  
 }
 
 function displayGuest() {
     const guestListElement = document.getElementById("guestList");
-    guestListElement.innerHTML = " ";
+    guestListElement.innerHTML = " "; 
 
     guestList.forEach((guest, index) => {
-        const listItem = document.createElement("li");
+        const listItem = document.createElement("li"); //creating a new element called "li"
         listItem.textContent = guest;
 
         const removeBtn = document.createElement("button");
         removeBtn.textContent = "Remove";
         removeBtn.onclick = () => removeGuest(index);
 
-        listItem.appendChild(removeBtn);
+        listItem.appendChild(removeBtn); //creating a remove button for each guest
         guestListElement.appendChild(listItem);
     })
 }
+ 
 
 function removeGuest(index) {
     guestList.splice(index , 1);
@@ -42,5 +45,5 @@ let Attending = false;
 
 function toggleRSVP() {
     Attending = Attending;
-    console.log(`RSVP status: ${Attending  "Attending" : "Not Attending" }`);
+    console.log(`RSVP status: ${Attending}  "Attending" : "Not Attending" `);
 }
